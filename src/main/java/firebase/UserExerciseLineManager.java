5091c1bc27193e1064d97951b8f003cbde20aab2
Alain <alain.guineaja@elorrieta-errekamari.com>
2025-10-18 14:41:55 +0200

    Creación del UserExerciseLineManager

---
+++ src/main/java/firebase/UserExerciseLineManager.java
@@ -0,0 +1,91 @@
+package firebase;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.CollectionReference;
+import com.google.cloud.firestore.DocumentReference;
+import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.QueryDocumentSnapshot;
+import com.google.cloud.firestore.QuerySnapshot;
+
+import entities.UserExerciseLine;
+import firebase.exceptions.DBException;
+
+public class UserExerciseLineManager implements ManagerInterface<UserExerciseLine> {
+	
+	private static UserExerciseLineManager instance = null;
+	
+	private String collectionName = null;
+	
+	public UserExerciseLineManager() {
+		this.collectionName = "userExerciseLines";
+	}
+	
+	public static UserExerciseLineManager getInstance() {
+		if (null == instance) {
+			instance = new UserExerciseLineManager();
+		}
+		return instance;
+	}
+
+	@Override
+	public List<UserExerciseLine> selectAll() throws DBException {
+		List<UserExerciseLine> ret = null;
+
+		try {
+			Firestore db = ManagerFactory.getInstance().getDB();
+			ApiFuture<QuerySnapshot> query = db.collection(collectionName).get();
+			QuerySnapshot qs = query.get();
+			List<QueryDocumentSnapshot> userExerciseLines = qs.getDocuments();
+			if (null == ret) ret = new ArrayList<UserExerciseLine>();
+			for (QueryDocumentSnapshot userExerciseLine: userExerciseLines) {
+				ret.add(new UserExerciseLine(
+					userExerciseLine.getLong("userId").intValue(),
+					userExerciseLine.getLong("exerciseId").intValue()
+				));
+			}
+		} catch (Exception ex) {
+			throw new DBException();
+		}
+
+		return ret;
+	}
+
+	@Override
+	public void insert(UserExerciseLine t) throws DBException {
+		Firestore db = ManagerFactory.getInstance().getDB();
+		CollectionReference query = db.collection(this.collectionName);
+
+		Map<String, Object> userExerciseLineMap = new HashMap<>();
+		userExerciseLineMap.put("userId",     t.getUserId());
+		userExerciseLineMap.put("exerciseId", t.getExerciseId());
+
+		DocumentReference dr = query.document(String.format("%dx%d", t.getUserId(), t.getExerciseId()));
+		dr.set(userExerciseLineMap);
+	}
+
+	@Override
+	public void update(UserExerciseLine t) throws DBException {
+		Firestore db = ManagerFactory.getInstance().getDB();
+		DocumentReference dr = db.collection(collectionName).document(String.format("%dx%d", t.getUserId(), t.getExerciseId()));
+
+		Map<String, Object> userExerciseLineMap = new HashMap<>();
+		userExerciseLineMap.put("userId",     t.getUserId());
+		userExerciseLineMap.put("exerciseId", t.getExerciseId());
+
+		dr.update(userExerciseLineMap);
+	}
+
+	@Override
+	public void delete(UserExerciseLine t) throws DBException {
+		Firestore db = ManagerFactory.getInstance().getDB();
+		DocumentReference dr = db.collection(collectionName).document(String.format("%dx%d", t.getUserId(), t.getExerciseId()));
+		dr.delete();
+	}
+	
+	
+}
