c79ad196bc763e6e87a2a34e45211a733650cef1
haritz <haritz.rodriguezro@elorrieta-errekamari.com>
2025-10-27 17:09:03 +0100

    Creación del método getWorkoutsByLevel

--- src/main/java/controllers/WorkoutController.java
+++ src/main/java/controllers/WorkoutController.java
@@ -1,5 +1,13 @@
 package controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import entities.User;
+import entities.Workout;
+import firebase.ManagerFactory;
+import firebase.exceptions.DBException;
+
 public class WorkoutController {
 	
 	private static WorkoutController instance = null;
@@ -12,4 +20,21 @@ public class WorkoutController {
 		}
 		return instance;
 	}
+	
+	public List<Workout> getWorkoutsByLevel(User user)
+	throws DBException {
+		List<Workout> ret = new ArrayList<Workout>();
+
+		List<Workout> workouts = ManagerFactory.getInstance().getWorkoutManager().selectAll();
+		
+		for (int i = 0; i < workouts.size(); i++) {
+
+			if (workouts.get(i).getLevel() <= user.getLevel()) {
+				ret.add(workouts.get(i));
+			}
+		}
+	
+		return ret;
+	}
+	
 }
