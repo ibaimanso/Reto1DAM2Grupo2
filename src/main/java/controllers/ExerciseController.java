0eb6f063e0732a0ffec260a2960307f873e107df
haritz <haritz.rodriguezro@elorrieta-errekamari.com>
2025-10-27 18:59:22 +0100

    Creación del método getExercisesByWorkout

--- src/main/java/controllers/ExerciseController.java
+++ src/main/java/controllers/ExerciseController.java
@@ -1,5 +1,13 @@
 package controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import entities.Exercise;
+import entities.Workout;
+import firebase.ManagerFactory;
+import firebase.exceptions.DBException;
+
 public class ExerciseController {
 	
 	private static ExerciseController instance = null;
@@ -12,4 +20,21 @@ public class ExerciseController {
 		}
 		return instance;
 	}
+	
+	public List<Exercise> getExercisesByWorkout(Workout workout)
+	throws DBException {
+		List<Exercise> ret = new ArrayList<Exercise>();
+
+		List<Exercise> exercises = ManagerFactory.getInstance().getExerciseManager().selectAll();
+		
+		for (int i = 0; i < exercises.size(); i++) {
+
+			if (exercises.get(i).getWorkoutId() == workout.getId()) {
+				ret.add(exercises.get(i));
+			}
+		}
+	
+		return ret;
+	}
+	
 }
