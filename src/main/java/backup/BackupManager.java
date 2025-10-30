ee4fc433813c9f68c6928884b0fd79e5118bb384
Alain <alain.guineaja@elorrieta-errekamari.com>
2025-10-30 15:08:38 +0100

    backup update

--- src/main/java/backup/BackupManager.java
+++ src/main/java/backup/BackupManager.java
@@ -2,8 +2,21 @@ package backup;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.List;
+
+import org.threeten.bp.LocalDateTime;
+import org.threeten.bp.format.DateTimeFormatter;
 
 import binary.ObjectFileManager;
+import entities.Exercise;
+import entities.Serie;
+import entities.User;
+import entities.UserExerciseLine;
+import entities.UserSerieLine;
+import entities.UserWorkoutLine;
+import entities.Workout;
+import firebase.ManagerFactory;
+import firebase.exceptions.DBException;
 import xml.ManagerFactoryXML;
 import xml.exceptions.XMLException;
 
@@ -23,19 +36,180 @@ public class BackupManager {
 		return instance;
 	}
 	
+	private User getUserById(List<User> users, User user) {
+		for (int index = 0; index < users.size(); index++) {
+			if (user.getId() == users.get(index).getId()) {
+				return users.get(index);
+			}
+		}
+		return null;
+	}
+	
 	public void tryBackup() {
 		
 		// TODO obtener las fechas de actualización y subir o bajar los datos en base a cual sea más reciente.
 		
+		List<User> localUsers  = null;
+		List<User> onlineUsers = null;
+		
+		try {
+			onlineUsers = ManagerFactory.getInstance().getUserManager().selectAll();
+		} catch (DBException e) {
+			System.exit(1);
+		}
+		
+		try {
+			localUsers = ManagerFactoryXML.getInstance().getUserManager().selectAll();
+		} catch (XMLException e) {
+			System.exit(2);
+		}
+		
+		for (int index = 0; index < onlineUsers.size(); index++) {
+			User localUser  = null;
+			User onlineUser = null;
+			
+			onlineUser = onlineUsers.get(index);
+			
+			localUser = getUserById(localUsers, onlineUser);
+			
+			if (null == localUser) {
+				this.makeLocalBackup(onlineUser);
+			} else {
+				LocalDateTime localDate  = LocalDateTime.parse(localUser.getLastMod(),  DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+				LocalDateTime onlineDate = LocalDateTime.parse(onlineUser.getLastMod(), DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+				
+				if (localDate.isBefore(onlineDate)) {
+					this.makeLocalBackup(localUser);
+				} else if (onlineDate.isBefore(localDate)) {
+					this.makeOnlineBackup(onlineUser);
+				}
+			}
+		}
+
+		List<Workout>  onlineWorkouts  = null;
+		List<Exercise> onlineExercises = null;
+		List<Serie>    onlineSeries    = null;
+
+		List<Workout>  localWorkouts   = null;
+		List<Exercise> localExercises  = null;
+		List<Serie>    localSeries     = null;
+		
+		try {
+			onlineWorkouts  = ManagerFactory.getInstance().getWorkoutManager().selectAll();
+			onlineExercises = ManagerFactory.getInstance().getExerciseManager().selectAll();
+			onlineSeries    = ManagerFactory.getInstance().getSerieManager().selectAll();
+			
+			if (
+				null == onlineWorkouts  ||
+				null == onlineExercises ||
+				null == onlineSeries
+			) {
+				System.exit(4);
+			}
+			
+			localWorkouts  = ManagerFactoryXML.getInstance().getWorkoutManager().selectAll();
+			localExercises = ManagerFactoryXML.getInstance().getExerciseManager().selectAll();
+			localSeries    = ManagerFactoryXML.getInstance().getSerieManager().selectAll();
+			
+			if (
+				null == localWorkouts  ||
+				null == localExercises ||
+				null == localSeries
+			) {
+				System.exit(5);
+			}
+			
+			for (Workout workout: localWorkouts)
+				ManagerFactoryXML.getInstance().getWorkoutManager().delete(workout);
+			for (Exercise exercise: localExercises)
+				ManagerFactoryXML.getInstance().getExerciseManager().delete(exercise);
+			for (Serie serie: localSeries)
+				ManagerFactoryXML.getInstance().getSerieManager().delete(serie);
+			
+			for (Workout workout: onlineWorkouts)
+				ManagerFactoryXML.getInstance().getWorkoutManager().insert(workout);
+			for (Exercise exercise: onlineExercises)
+				ManagerFactoryXML.getInstance().getExerciseManager().insert(exercise);
+			for (Serie serie: onlineSeries)
+				ManagerFactoryXML.getInstance().getSerieManager().insert(serie);
+			
+		} catch (Exception ex) {
+			System.exit(3);
+		}
+		
 		try {
 			this.makeBinaryBackup();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (XMLException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
+		} catch (Exception ex) {
+			System.exit(100);
 		}
+	}
+	
+	private UserData getUserWorkoutLinesByUserId(
+		User user,
+		List<UserWorkoutLine> userWorkoutLines,
+		List<UserExerciseLine> userExerciseLines,
+		List<UserSerieLine> userSerieLines
+	) {
+		UserData userData = new UserData();
+		
+		
+		return null;
+	}
+	
+	private void makeLocalBackup(User localUser) {
+		User onlineUser = null;
+
+		List<UserWorkoutLine>  onlineUserWorkoutLines  = null;
+		List<UserExerciseLine> onlineUserExerciseLines = null;
+		List<UserSerieLine>    onlineUserSerieLines    = null;
+
+		List<UserWorkoutLine>  localUserWorkoutLines   = null;
+		List<UserExerciseLine> localUserExerciseLines  = null;
+		List<UserSerieLine>    localUserSerieLines     = null;
+		
+		try {
+			onlineUserWorkoutLines  = ManagerFactory.getInstance().getUserWorkoutLineManager().selectAll();
+			onlineUserExerciseLines = ManagerFactory.getInstance().getUserExerciseLineManager().selectAll();
+			onlineUserSerieLines    = ManagerFactory.getInstance().getUserSerieLineManager().selectAll();
+		} catch (Exception ex) {
+			System.exit(6);
+		}
+		
+		try {
+			localUserWorkoutLines  = ManagerFactoryXML.getInstance().getUserWorkoutLineManager().selectAll();
+			localUserExerciseLines = ManagerFactoryXML.getInstance().getUserExerciseLineManager().selectAll();
+			localUserSerieLines    = ManagerFactoryXML.getInstance().getUserSerieLineManager().selectAll();
+		} catch (Exception ex) {
+			System.exit(7);
+		}
+		
+		/*
+		try {
+			for (UserWorkoutLine uwl: localUserWorkoutLines)
+				ManagerFactory.getInstance().getUserWorkoutLineManager().delete(uwl);
+			for (UserExerciseLine uel: localUserExerciseLines)
+				ManagerFactory.getInstance().getUserExerciseLineManager().delete(uel);
+			for (UserSerieLine usl: localUserSerieLines)
+				ManagerFactory.getInstance().getUserSerieLineManager().delete(usl);
+		} catch (Exception ex) {
+			System.exit(8);
+		}
+		
+		try {
+			for (UserWorkoutLine uwl: onlineUserWorkoutLines)
+				ManagerFactory.getInstance().getUserWorkoutLineManager().insert(uwl);
+			for (UserExerciseLine uel: onlineUserExerciseLines)
+				ManagerFactory.getInstance().getUserExerciseLineManager().insert(uel);
+			for (UserSerieLine usl: onlineUserSerieLines)
+				ManagerFactory.getInstance().getUserSerieLineManager().insert(usl);
+		} catch (Exception ex) {
+			System.exit(9);
+		}
+		*/
+	}
+
+	private void makeOnlineBackup(User onlineUser) {
+		User localUser = null;
 	}
 	
 	private void makeBinaryBackup()
@@ -52,4 +226,11 @@ public class BackupManager {
 		
 		new ObjectFileManager<BinData>().write(bd, binPath);
 	}
+}
+
+class UserData {
+	public User user = null;
+	public List<UserWorkoutLine>  userWorkoutLines  = null;
+	public List<UserExerciseLine> userExerciseLines = null;
+	public List<UserSerieLine>    userSerieLines    = null;
 }
